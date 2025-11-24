95768e61cc34bd96ba2f2fb7a5e41180a6d15bbd
sullacarlvinjay <sullacarlvinjay@gmail.com>
2025-11-24 21:49:18 +0800

    Fix STATICFILES_STORAGE to prevent deployment error
    - Changed from None to proper StaticFilesStorage
    - Fixes AttributeError: 'NoneType' object has no attribute 'rsplit'
    - WhiteNoise can now serve static files properly in production

--- myproject/settings.py
+++ myproject/settings.py
@@ -99,8 +99,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-# Don't use staticfiles collection, serve directly from static/
-STATICFILES_STORAGE = None
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
